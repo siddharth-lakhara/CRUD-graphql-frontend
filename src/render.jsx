87deedafc8cc00c4a0db4bcd98484844f980d718
lakhara <siddharth.lakhara@gmail.com>
2018-08-16 11:26:24 +0530

    boiler plate

---
+++ src/render.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloClient, createNetworkInterface, ApolloProvider } from 'react-apollo';
+
+import './index.css';
+import App from './App';
+
+
+const render = () => {
+  const networkInterface = createNetworkInterface({
+    uri: 'http://localhost:8080/graphql',
+  });
+
+  const client = new ApolloClient({
+    networkInterface,
+  });
+
+  const renderComponent = (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  );
+  ReactDOM.render(renderComponent, document.getElementById('root'));
+};
+
+export default render;
