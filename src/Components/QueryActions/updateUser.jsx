d122af2e6f13fe91f44507fb097ac9b215e41d22
lakhara <siddharth.lakhara@gmail.com>
2018-08-16 17:37:34 +0530

    update user

--- src/Components/QueryActions/updateUser.jsx
+++ src/Components/QueryActions/updateUser.jsx
@@ -1,10 +1,99 @@
 import React from 'react';
+import { gql, graphql } from 'react-apollo';
+import validateInput from './utils/validateInput';
+import renderErrors from './utils/renderErrors';
 
-const UpdateUser = () => {
-  const some = 1;
-  return (
-    <button type="button">Update User</button>
-  );
-};
+const updateUserRequest = gql`
+  mutation($userName: String!, $email: String!) {
+    updateUser(userName: $userName, email: $email) {
+      ok
+      errors {
+        path
+        message
+      }
+    }
+}
+`;
 
-export default UpdateUser;
+class updateUser extends React.Component {
+  state = {
+    userName: '',
+    email: '',
+  };
+
+  sendRequest = async () => {
+    const { userName, email } = this.state;
+    const { mutate, fetchResults } = this.props;
+    const v = validateInput({
+      userName,
+      email,
+    });
+    if (!v.error) {
+      const response = await mutate({
+        variables: { userName, email },
+      });
+      const { ok, errors } = response.data.updateUser;
+      if (ok) {
+        fetchResults(
+          <div className="ok"> User updated succesfully!</div>,
+        );
+      } else {
+        fetchResults(
+          <div className="error">
+            {renderErrors(errors)}
+          </div>,
+        );
+      }
+    } else {
+      fetchResults(
+        <div className="error">
+          Error:
+          {' '}
+          {v.error.message}
+        </div>,
+      );
+    }
+  };
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    const { userName, email } = this.state;
+    const { handleChange, sendRequest } = this;
+    return (
+      <div className="createUser-main">
+        <input
+          type="text"
+          name="userName"
+          value={userName}
+          placeholder="Enter new user name"
+          onChange={handleChange}
+          className="createUser-input"
+        />
+        <input
+          type="text"
+          name="email"
+          value={email}
+          placeholder="Enter email"
+          onChange={handleChange}
+          className="createUser-input"
+        />
+        <button
+          type="button"
+          className="createUser-submit"
+          onClick={sendRequest}
+        >
+          Update User
+        </button>
+      </div>
+
+    );
+  }
+}
+
+export default graphql(updateUserRequest)(updateUser);
